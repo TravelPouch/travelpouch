e6b6b809e2790eef397c872353d55b9ddd37d833
tibiscuit <antoine.pelletier@epfl.ch>
2024-10-08 20:00:53 +0200

    feat(viewmodel): add ListTravelViewModel with CRUD operations
    - Implemented ListTravelViewModel with methods to get, add, update, and delete Travel documents.
    - Integrated repository calls for each CRUD operation.
    - Added StateFlow properties to manage the state of travels and selected travel.

---
+++ app/src/main/java/com/github/se/travelpouch/model/ListTravelViewModel.kt
@@ -0,0 +1,59 @@
+package com.github.se.travelpouch.model
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+open class ListTravelViewModel(private val repository: TravelRepository) : ViewModel() {
+  private val travels_ = MutableStateFlow<List<TravelContainer>>(emptyList())
+  val travels: StateFlow<List<TravelContainer>> = travels_.asStateFlow()
+
+  private val selectedTravel_ = MutableStateFlow<TravelContainer?>(null)
+  open val selectedTravel: StateFlow<TravelContainer?> = selectedTravel_.asStateFlow()
+
+  init {
+    repository.init { getTravels() }
+  }
+
+  /**
+   * Generates a new unique ID.
+   *
+   * @return A new unique ID.
+   */
+  fun getNewUid(): String {
+    return repository.getNewUid()
+  }
+
+  /** Gets all Travel documents. */
+  fun getTravels() {
+    repository.getTravels(onSuccess = { travels_.value = it }, onFailure = {})
+  }
+
+  /**
+   * Adds a Travel document.
+   *
+   * @param travel The Travel document to be added.
+   */
+  fun addTravel(travel: TravelContainer) {
+    repository.addTravel(travel = travel, onSuccess = { getTravels() }, onFailure = {})
+  }
+
+  /**
+   * Updates a Travel document.
+   *
+   * @param travel The Travel document to be updated.
+   */
+  fun updateTravel(travel: TravelContainer) {
+    repository.updateTravel(travel = travel, onSuccess = { getTravels() }, onFailure = {})
+  }
+
+  /**
+   * Deletes a Travel document.
+   *
+   * @param id The ID of the Travel document to be deleted.
+   */
+  fun deleteTravelById(id: String) {
+    repository.deleteTravelById(id = id, onSuccess = { getTravels() }, onFailure = {})
+  }
+}
