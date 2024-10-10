5f0aa854a442d7106b1c921e9e687e886681057e
appellet <160597306+appellet@users.noreply.github.com>
2024-10-10 23:23:06 +0200

    fix(travel-repository): add toMap method and correct time fields (#39)
    feat(travel-repository): add toMap method and correct time fields
    
    - Added `toMap` method to `TravelRepositoryFirestore` to facilitate serialization.
    - Corrected `endTime` and `startTime` fields in `documentToTravel` method.
    - Updated unit tests to reflect these changes and ensure functionality.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     alias(libs.plugins.jetbrainsKotlinAndroid)
     alias(libs.plugins.ktfmt)
     alias(libs.plugins.sonar)
-    id("jacoco")
+    jacoco
     alias(libs.plugins.gms)
 }
 
