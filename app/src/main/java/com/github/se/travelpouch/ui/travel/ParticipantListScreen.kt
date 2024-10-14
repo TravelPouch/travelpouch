3432a818e160a403f975aed7ff6d7704957c8064
Remismywaifuu <victor.rydmark@epfl.ch>
2024-10-14 20:28:59 +0200

    feat: Add user edit and deletion popup
    Implement display logic to change roles of participants
    as well as removing them from the Travel.
    No actual data is modified.

--- app/src/main/java/com/github/se/travelpouch/ui/travel/ParticipantListScreen.kt
+++ app/src/main/java/com/github/se/travelpouch/ui/travel/ParticipantListScreen.kt
@@ -1,13 +1,16 @@
 package com.github.se.travelpouch.ui.travel
 
+import TruncatedText
 import android.annotation.SuppressLint
-import android.widget.Toast
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
@@ -24,6 +27,8 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -31,11 +36,13 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 import com.github.se.travelpouch.model.ListTravelViewModel
 import com.github.se.travelpouch.model.Participant
+import com.github.se.travelpouch.model.UserInfo
+import com.github.se.travelpouch.model.fsUid
 import com.github.se.travelpouch.ui.navigation.NavigationActions
 
-// parts of this file was generated using Github Copilot
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -47,6 +54,10 @@ fun ParticipantListScreen(
   val participants by listTravelViewModel.participants.collectAsState()
 
   val context = LocalContext.current
+  val (expanded, setExpanded) = remember { mutableStateOf(false) }
+  val (expandedRoleDialog, setExpandedRoleDialog) = remember { mutableStateOf(false) }
+  val (selectedParticipant, setSelectedParticipant) =
+      remember { mutableStateOf<Map.Entry<fsUid, UserInfo>?>(null) }
 
   Scaffold(
       modifier = Modifier.testTag("listScreen"),
@@ -72,34 +83,16 @@ fun ParticipantListScreen(
       },
   ) { paddingValues ->
     if (selectedTravel != null) {
-
-      LazyColumn(modifier = Modifier.padding(paddingValues).fillMaxSize()) {
-        participants.entries.forEach { participant ->
+      LazyColumn(modifier = Modifier.padding(paddingValues).fillMaxWidth()) {
+        participants.entries.forEach { participantEntry ->
           item {
-            Row(
-                modifier =
-                    Modifier.fillMaxWidth().clickable {
-                      Toast.makeText(context, "Participant clicked", Toast.LENGTH_SHORT).show()
-                    },
-                horizontalArrangement = Arrangement.SpaceBetween,
-                verticalAlignment = Alignment.CenterVertically) {
-                  Icon(
-                      imageVector = Icons.Default.Person,
-                      contentDescription = "Localized description",
-                      modifier = Modifier.padding(10.dp))
-                  Text(
-                      text = participant.value.name,
-                      fontWeight = FontWeight.Bold,
-                      modifier = Modifier.padding(10.dp))
-                  Text(
-                      text = participant.value.email,
-                      fontWeight = FontWeight.Bold,
-                      modifier = Modifier.padding(10.dp))
-                  Text(
-                      text = selectedTravel!!.allParticipants[Participant(participant.key)]!!.name,
-                      fontWeight = FontWeight.Bold,
-                      modifier = Modifier.padding(10.dp))
-                }
+            ParticipantRow(
+                participant = participantEntry,
+                selectedTravel = selectedTravel!!,
+                onClick = {
+                  setSelectedParticipant(participantEntry)
+                  setExpanded(true)
+                })
             HorizontalDivider(thickness = 0.5.dp, color = Color.Gray)
           }
         }
@@ -108,6 +101,95 @@ fun ParticipantListScreen(
       Text(
           "No Travel is selected or the selected travel no longer exists.",
           modifier = Modifier.padding(15.dp).testTag("noTravelSelected"))
+    }
+
+    selectedParticipant?.let { participant ->
+      if (expanded) {
+        Dialog(onDismissRequest = { setExpanded(false) }) {
+          Box(Modifier.size(2000.dp, 200.dp).background(Color.White)) {
+            Column(modifier = Modifier.padding(8.dp)) {
+              Row(
+                  verticalAlignment = Alignment.CenterVertically,
+                  horizontalArrangement = Arrangement.SpaceBetween) {
+                    Icon(
+                        imageVector = Icons.Default.Person,
+                        contentDescription = "Localized description",
+                        modifier = Modifier.padding(5.dp))
+                    TruncatedText(
+                        text = participant.value.name,
+                        fontWeight = FontWeight.Bold,
+                        maxLength = 20,
+                        modifier = Modifier.padding(5.dp))
+                    TruncatedText(
+                        text = participant.value.email,
+                        maxLength = 20,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier.padding(5.dp))
+                  }
+
+              Text(
+                  text =
+                      "Role : ${selectedTravel!!.allParticipants[Participant(participant.key)]!!.name}",
+                  fontWeight = FontWeight.Bold,
+                  modifier = Modifier.padding(8.dp))
+              Button(onClick = { setExpandedRoleDialog(true) }) { Text("Change Role") }
+
+              Button(
+                  onClick = {
+                    // Handle Remove participant
+                    setExpanded(false)
+                  }) {
+                    Text("Remove participant")
+                  }
+            }
+          }
+        }
+      }
+
+      if (expandedRoleDialog) {
+        Dialog(onDismissRequest = { setExpandedRoleDialog(false) }) {
+          Box(Modifier.size(800.dp, 250.dp).background(Color.White)) {
+            Column(
+                modifier = Modifier.fillMaxSize().padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center) {
+                  Text(
+                      "Select a Role",
+                      fontWeight = FontWeight.Bold,
+                      modifier = Modifier.padding(8.dp))
+                  Text(
+                      "Current Role: ${selectedTravel!!.allParticipants[Participant(participant.key)]!!.name}",
+                      modifier = Modifier.padding(8.dp))
+                  Button(
+                      onClick = {
+                        // Handle OWNER selection
+                        setExpanded(false)
+                        setExpandedRoleDialog(false)
+                      }) {
+                        Text("OWNER")
+                      }
+
+                  Button(
+                      onClick = {
+                        // Handle ORGANISER selection
+                        setExpanded(false)
+                        setExpandedRoleDialog(false)
+                      }) {
+                        Text("ORGANISER")
+                      }
+
+                  Button(
+                      onClick = {
+                        // Handle PARTICIPANT selection
+                        setExpanded(false)
+                        setExpandedRoleDialog(false)
+                      }) {
+                        Text("PARTICIPANT")
+                      }
+                }
+          }
+        }
+      }
     }
   }
 }
