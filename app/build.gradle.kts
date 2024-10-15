05c83023aa45bf3cb6c0778ed1bbb6e77abc3db0
appellet <antoine.pelletier@epfl.ch>
2024-10-15 17:51:58 +0200

    chore: update Jacoco plugin declaration in build.gradle.kts
    This commit updates the Jacoco plugin declaration in the `build.gradle.kts` file to use `id("jacoco")` instead of just `jacoco`. This change ensures proper plugin configuration and compatibility.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     alias(libs.plugins.jetbrainsKotlinAndroid)
     alias(libs.plugins.ktfmt)
     alias(libs.plugins.sonar)
-    jacoco
+    id("jacoco")
     alias(libs.plugins.gms)
 }
 
