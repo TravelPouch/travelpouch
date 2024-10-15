a0a63a246efe0a6c89e5eefaf2a88c8c51add6da
appellet <antoine.pelletier@epfl.ch>
2024-10-15 14:09:22 +0200

    chore: apply ktfmt formatting to all code
    This commit applies ktfmt formatting to the entire codebase to ensure consistent code style and formatting. All Kotlin files have been reformatted according to the ktfmt guidelines.

--- app/src/androidTest/java/com/github/se/travelpouch/MainActivityTest.kt
+++ app/src/androidTest/java/com/github/se/travelpouch/MainActivityTest.kt
@@ -1,8 +1,6 @@
 package com.github.se.travelpouch
 
-import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
-import androidx.compose.ui.test.onNodeWithTag
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Rule
 import org.junit.Test
@@ -13,7 +11,5 @@ class MainActivityTest {
 
   @get:Rule val composeTestRule = createAndroidComposeRule<MainActivity>()
 
-  @Test
-  fun testMainActivityDisplaysCorrectContent() {
-  }
+  @Test fun testMainActivityDisplaysCorrectContent() {}
 }
