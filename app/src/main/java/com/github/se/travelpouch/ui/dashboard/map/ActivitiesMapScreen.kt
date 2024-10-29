0f826aeee915ff6486b835c6a9807ffa10976155
Alexandre Spiess <alexandre.spiess@epfl.ch>
2024-10-29 10:23:32 +0100

    refactor: add Google map in a composable

--- app/src/main/java/com/github/se/travelpouch/ui/dashboard/map/ActivitiesMapScreen.kt
+++ app/src/main/java/com/github/se/travelpouch/ui/dashboard/map/ActivitiesMapScreen.kt
@@ -2,6 +2,7 @@ package com.github.se.travelpouch.ui.dashboard.map
 
 import android.util.Log
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
@@ -17,10 +18,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.unit.dp
+import com.github.se.travelpouch.model.activity.Activity
 import com.github.se.travelpouch.model.activity.ActivityViewModel
 import com.github.se.travelpouch.ui.navigation.NavigationActions
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.CameraPositionState
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.rememberCameraPositionState
@@ -69,26 +72,7 @@ fun ActivitiesMapScreen(
         // Use a Box to overlay the button on top of the map
         Box(modifier = Modifier.padding(paddingValues)) {
           // Display the Google Map with markers for each activity
-          // Display the Google Map with markers for each activity
-          GoogleMap(
-              modifier = Modifier.padding(paddingValues).testTag("Map"),
-              cameraPositionState = cameraPositionState) {
-                // Add a marker for each activity's location
-                listOfActivities.forEach { activity ->
-                  activity.location.let { location -> // Ensure location is not null
-                    Marker(
-                        state =
-                            rememberMarkerState(
-                                position =
-                                    LatLng(
-                                        location.latitude.toDouble(),
-                                        location.longitude.toDouble())),
-                        title = activity.title, // The title of the activity
-                        snippet = activity.description, // The description of the activity
-                        contentDescription = "Marker for ${activity.title}")
-                  }
-                }
-              }
+          Map(listOfActivities, paddingValues, cameraPositionState)
 
           // Add a floating action button for going back
           FloatingActionButton(
@@ -102,4 +86,29 @@ fun ActivitiesMapScreen(
               }
         }
       })
+}
+
+@Composable
+fun Map(
+    listOfActivities: List<Activity>,
+    paddingValues: PaddingValues,
+    cameraPositionState: CameraPositionState
+) {
+  GoogleMap(
+      modifier = Modifier.padding(paddingValues).testTag("Map"),
+      cameraPositionState = cameraPositionState) {
+        // Add a marker for each activity's location
+        listOfActivities.forEach { activity ->
+          activity.location.let { location -> // Ensure location is not null
+            Marker(
+                state =
+                    rememberMarkerState(
+                        position =
+                            LatLng(location.latitude.toDouble(), location.longitude.toDouble())),
+                title = activity.title, // The title of the activity
+                snippet = activity.description, // The description of the activity
+                contentDescription = "Marker for ${activity.title}")
+          }
+        }
+      }
 }
