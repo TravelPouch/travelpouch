144fbc30b06921f2749c19996e5661d91d0596bb
Sylvain Nérisson <sylvain.nerisson@epfl.ch>
2024-11-01 05:00:48 +0100

    feat: display the PDF preview in the document list

--- app/src/main/java/com/github/se/travelpouch/ui/documents/DocumentListItem.kt
+++ app/src/main/java/com/github/se/travelpouch/ui/documents/DocumentListItem.kt
@@ -1,7 +1,10 @@
 package com.github.se.travelpouch.ui.documents
 
+import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -11,6 +14,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -22,6 +26,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.text.font.FontWeight
@@ -30,6 +35,9 @@ import coil.compose.AsyncImage
 import com.github.se.travelpouch.model.documents.DocumentContainer
 import com.github.se.travelpouch.model.documents.DocumentFileFormat
 import com.github.se.travelpouch.model.documents.DocumentViewModel
+import com.rizzi.bouquet.ResourceType
+import com.rizzi.bouquet.VerticalPDFReader
+import com.rizzi.bouquet.rememberVerticalPdfReaderState
 import java.text.SimpleDateFormat
 import java.util.Locale
 
@@ -98,10 +106,12 @@ fun DocumentListItem(
 @Composable
 fun DocumentPreviewBox(previewUri: String, fileFormat: DocumentFileFormat) {
   if (fileFormat == DocumentFileFormat.PDF) {
-    Text(
-        text = "PDF Preview Not Implemented",
-        style = MaterialTheme.typography.bodyMedium,
-        modifier = Modifier.testTag("DocumentPreviewBoxNotImpl"))
+    val pdfState =
+      rememberVerticalPdfReaderState(
+        resource = ResourceType.Remote(previewUri), isZoomEnable = false)
+    VerticalPDFReader(
+      state = pdfState,
+      modifier = Modifier.fillMaxSize().background(color = Color.Gray))
   } else {
     AsyncImage(
         model = previewUri,
