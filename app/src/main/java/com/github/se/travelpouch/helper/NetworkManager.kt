1947fd23bf7adac39586b83dccfd1815d8a26ba7
Alexandre Spiess <alexandre.spiess@epfl.ch>
2024-11-04 09:40:14 +0100

    format: format to ktfmt format for NetworkManager

--- app/src/main/java/com/github/se/travelpouch/helper/NetworkManager.kt
+++ app/src/main/java/com/github/se/travelpouch/helper/NetworkManager.kt
@@ -11,46 +11,46 @@ import okhttp3.Response
 /** A helper class to manage network calls and handle responses. */
 class NetworkManager(private val client: OkHttpClient) {
 
-    /**
-     * Makes a network call and handles the response or failure.
-     *
-     * @param request The HTTP request to be executed.
-     * @param onSuccess Callback that is called when the request is successful.
-     * @param onFailure Callback that is called when the request fails.
-     */
-    fun executeRequest(
-        request: Request,
-        onSuccess: (String) -> Unit,
-        onFailure: (Exception) -> Unit
-    ) {
-        client
-            .newCall(request)
-            .enqueue(
-                object : Callback {
-                    override fun onFailure(call: Call, e: IOException) {
-                        Log.e("NetworkManager", "Failed to execute request", e)
-                        onFailure(e)
-                    }
+  /**
+   * Makes a network call and handles the response or failure.
+   *
+   * @param request The HTTP request to be executed.
+   * @param onSuccess Callback that is called when the request is successful.
+   * @param onFailure Callback that is called when the request fails.
+   */
+  fun executeRequest(
+      request: Request,
+      onSuccess: (String) -> Unit,
+      onFailure: (Exception) -> Unit
+  ) {
+    client
+        .newCall(request)
+        .enqueue(
+            object : Callback {
+              override fun onFailure(call: Call, e: IOException) {
+                Log.e("NetworkManager", "Failed to execute request", e)
+                onFailure(e)
+              }
 
-                    override fun onResponse(call: Call, response: Response) {
-                        response.use {
-                            if (!response.isSuccessful) {
-                                val exception = Exception("Unexpected code $response")
-                                Log.d("NetworkManager", "Unexpected code $response")
-                                onFailure(exception)
-                                return
-                            }
+              override fun onResponse(call: Call, response: Response) {
+                response.use {
+                  if (!response.isSuccessful) {
+                    val exception = Exception("Unexpected code $response")
+                    Log.d("NetworkManager", "Unexpected code $response")
+                    onFailure(exception)
+                    return
+                  }
 
-                            val body = response.body?.string()
-                            if (body != null) {
-                                onSuccess(body)
-                            } else {
-                                val exception = Exception("Empty response body")
-                                Log.d("NetworkManager", "Empty body")
-                                onFailure(exception)
-                            }
-                        }
-                    }
-                })
-    }
+                  val body = response.body?.string()
+                  if (body != null) {
+                    onSuccess(body)
+                  } else {
+                    val exception = Exception("Empty response body")
+                    Log.d("NetworkManager", "Empty body")
+                    onFailure(exception)
+                  }
+                }
+              }
+            })
+  }
 }
