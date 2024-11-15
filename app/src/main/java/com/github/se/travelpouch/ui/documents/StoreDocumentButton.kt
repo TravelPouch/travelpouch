4359eabd7ff4bad0ffa27c71620fc541454997fe
Arthur Chalard <arthur.chalard@epfl.ch>
2024-11-15 07:00:39 +0100

    fix: remove unused imports

--- app/src/main/java/com/github/se/travelpouch/ui/documents/StoreDocumentButton.kt
+++ app/src/main/java/com/github/se/travelpouch/ui/documents/StoreDocumentButton.kt
@@ -9,8 +9,6 @@ import androidx.compose.material.icons.filled.FileDownload
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
 import com.github.se.travelpouch.model.documents.DocumentViewModel
