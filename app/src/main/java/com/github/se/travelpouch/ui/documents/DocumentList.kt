115c061d7bd614aae400034cf482f03fa7438414
Arthur Chalard <arthur.chalard@epfl.ch>
2024-11-29 10:26:34 +0100

    test: add e2e test for importing a file from local storage
    This is a first version with a kind of bug introduced in DocumentList

--- app/src/main/java/com/github/se/travelpouch/ui/documents/DocumentList.kt
+++ app/src/main/java/com/github/se/travelpouch/ui/documents/DocumentList.kt
@@ -122,7 +122,7 @@ fun DocumentListScreen(
             documentViewModel.uploadFile(
                 context.contentResolver.openInputStream(uri),
                 selectedTravel.value,
-                context.contentResolver.getType(uri))
+                context.contentResolver.getType(uri) ?: "image/png")
         else Log.d("DocumentList", "No file selected")
       }
 
