aa3e7fd78f53bba5c33f4d3d025ee028f1d61b5f
yassine04e <yassine.elgraoui@epfl.ch>
2024-12-04 19:02:57 +0100

    test: Incresing coverage by testing that the success callback works e^when seding a friend invitation to someone

--- app/src/androidTest/java/com/github/se/travelpouch/ui/profile/ProfileEditTest.kt
+++ app/src/androidTest/java/com/github/se/travelpouch/ui/profile/ProfileEditTest.kt
@@ -12,15 +12,26 @@ import com.github.se.travelpouch.model.notifications.NotificationViewModel
 import com.github.se.travelpouch.model.profile.Profile
 import com.github.se.travelpouch.model.profile.ProfileModelView
 import com.github.se.travelpouch.model.profile.ProfileRepository
+import com.github.se.travelpouch.model.profile.ProfileRepositoryFirebase
 import com.github.se.travelpouch.ui.navigation.NavigationActions
+import com.google.android.gms.tasks.OnSuccessListener
+import com.google.android.gms.tasks.Task
+import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
+import com.google.firebase.firestore.QuerySnapshot
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.`when`
 import org.mockito.kotlin.anyOrNull
+import org.mockito.kotlin.argumentCaptor
+import org.mockito.kotlin.eq
 import org.mockito.kotlin.never
 import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
 
 class ProfileEditTest {
   private lateinit var profileModelView: ProfileModelView
@@ -129,5 +140,55 @@ class ProfileEditTest {
     composeTestRule.onNodeWithTag("addingFriendButton").performClick()
 
     verify(profileRepository).sendFriendNotification(anyOrNull(), anyOrNull(), anyOrNull())
+  }
+
+  @Test
+  fun testThatSuccessMethodSendsNotification() {
+    val mockDatabase: FirebaseFirestore = mock()
+    val mockCollectionReference: CollectionReference = mock()
+    val mockQuery: Query = mock()
+    val taskQuerySnapshot: Task<QuerySnapshot> = mock()
+    val mockQuerySnapshot: QuerySnapshot = mock()
+    val mockDocumentSnapshot: DocumentSnapshot = mock()
+
+    whenever(mockDatabase.collection(anyOrNull())).thenReturn(mockCollectionReference)
+    whenever(mockCollectionReference.whereEqualTo(eq("email"), anyOrNull())).thenReturn(mockQuery)
+    whenever(mockQuery.get()).thenReturn(taskQuerySnapshot)
+
+    whenever(taskQuerySnapshot.isSuccessful).thenReturn(true)
+    whenever(taskQuerySnapshot.result).thenReturn(mockQuerySnapshot)
+    whenever(taskQuerySnapshot.addOnSuccessListener(anyOrNull())).thenReturn(taskQuerySnapshot)
+    whenever(taskQuerySnapshot.addOnFailureListener(anyOrNull())).thenReturn(taskQuerySnapshot)
+    whenever(mockQuerySnapshot.isEmpty).thenReturn(false)
+    whenever(mockQuerySnapshot.documents).thenReturn(listOf(mockDocumentSnapshot))
+
+    whenever(mockDocumentSnapshot.id).thenReturn("qwertzuiopasdfghjklyxcvbnm12")
+    whenever(mockDocumentSnapshot.getString("username")).thenReturn("username")
+    whenever(mockDocumentSnapshot.getString("email")).thenReturn("email@email.com")
+    whenever(mockDocumentSnapshot.get("friends")).thenReturn(emptyList<String>())
+    whenever(mockDocumentSnapshot.get("listoftravellinked")).thenReturn(emptyList<String>())
+    whenever(mockDocumentSnapshot.getString("name")).thenReturn("name")
+
+    whenever(notificationRepository.getNewUid()).thenReturn("qwertzuiopasdfghjkly")
+
+    val profileModelView = ProfileModelView(ProfileRepositoryFirebase(mockDatabase))
+
+    composeTestRule.setContent {
+      ModifyingProfileScreen(navigationActions, profileModelView, notificationViewModel)
+    }
+
+    composeTestRule.onNodeWithTag("floatingButtonAddingFriend").performClick()
+    composeTestRule.onNodeWithTag("addingFriendField").performTextClearance()
+    composeTestRule.onNodeWithTag("addingFriendField").performTextInput("email@email.com")
+    composeTestRule.onNodeWithTag("addingFriendButton").performClick()
+
+    val onCompleteListenerCaptor2 = argumentCaptor<OnSuccessListener<QuerySnapshot>>()
+    verify(taskQuerySnapshot).addOnSuccessListener(onCompleteListenerCaptor2.capture())
+
+    // This line was generated thanks to Chat-GPT. This allows us to display the Toast even if the
+    // piece of code is executed from the argument captor
+    composeTestRule.runOnIdle { onCompleteListenerCaptor2.firstValue.onSuccess(mockQuerySnapshot) }
+
+    verify(notificationRepository).addNotification(anyOrNull())
   }
 }
