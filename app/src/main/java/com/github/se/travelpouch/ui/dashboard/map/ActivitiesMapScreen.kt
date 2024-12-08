5e405d79bc026bfb96b3a9a3e8af4c44076c0330
Alexandre Spiess <alexandre.spiess@epfl.ch>
2024-12-08 11:41:24 +0100

    feat: add BottomSheet to display information ons

--- app/src/main/java/com/github/se/travelpouch/ui/dashboard/map/ActivitiesMapScreen.kt
+++ app/src/main/java/com/github/se/travelpouch/ui/dashboard/map/ActivitiesMapScreen.kt
@@ -2,13 +2,30 @@ package com.github.se.travelpouch.ui.dashboard.map
 
 import android.util.Log
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccessTime
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.CalendarToday
+import androidx.compose.material.icons.filled.Description
+import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.material3.BottomSheetScaffold
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberBottomSheetScaffoldState
+import androidx.compose.material3.rememberStandardBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -16,10 +33,12 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.unit.dp
@@ -46,6 +65,7 @@ import com.google.maps.android.compose.rememberCameraPositionState
 import com.google.maps.android.compose.rememberMarkerState
 import java.text.SimpleDateFormat
 import java.util.Locale
+import kotlinx.coroutines.launch
 
 /**
  * Composable function that displays a map screen showing all activities. The activities are marked
@@ -54,11 +74,12 @@ import java.util.Locale
  * @param activityViewModel The ViewModel containing the list of activities.
  * @param navigationActions Navigation actions for managing app navigation.
  */
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ActivitiesMapScreen(
     activityViewModel: ActivityViewModel,
     navigationActions: NavigationActions,
-    directionsViewModel: DirectionsViewModel
+    directionsViewModel: DirectionsViewModel // TODO : remove this
 ) {
 
   // Collect the list of activities from the ViewModel
@@ -119,15 +140,28 @@ fun ActivitiesMapScreen(
   // Update the camera position when the valid activities change
   CameraUpdater(validActivities, cameraPositionState)
 
-  Scaffold(
-      modifier = Modifier.testTag("ActivityMapScreen"),
-      content = { paddingValues ->
+  val bottomSheetScaffoldState =
+      rememberBottomSheetScaffoldState(
+          bottomSheetState =
+              rememberStandardBottomSheetState(
+                  skipHiddenState = false // Ensure this is set to false
+                  ))
+
+  var bottomSheetContent by remember { mutableStateOf<@Composable () -> Unit>({}) }
+
+  val scope = rememberCoroutineScope()
+
+  BottomSheetScaffold(
+      scaffoldState = bottomSheetScaffoldState,
+      sheetPeekHeight = 0.dp,
+      sheetContent = { Box(modifier = Modifier.fillMaxWidth()) { bottomSheetContent() } }) {
+          innerPadding ->
 
         // Use a Box to overlay the button on top of the map
-        Box(modifier = Modifier.padding(paddingValues)) {
+        Box(modifier = Modifier.padding(innerPadding)) {
           // Display the Google Map with markers for each activity
           GoogleMap(
-              modifier = Modifier.padding(paddingValues).testTag("Map"),
+              modifier = Modifier.fillMaxSize().testTag("Map"),
               cameraPositionState = cameraPositionState) {
                 AddCurrentLocationMarker(currentLocation)
 
@@ -135,14 +169,31 @@ fun ActivitiesMapScreen(
                     activities = validActivities,
                     dateFormat = dateFormat,
                     selectedActivity = selectedActivity,
-                    onActivitySelected = { selectedActivity = it })
+                    onActivitySelected = {
+                      selectedActivity = it
+
+                      bottomSheetContent = { ActivityDetailsContent(it) }
+                      scope.launch { bottomSheetScaffoldState.bottomSheetState.expand() }
+                    })
 
                 DrawActivitiesPaths(
                     activitiesRouteDetails = activitiesRouteDetails,
                     selectedRouteIndex = selectedRouteIndex,
-                    onRouteSelected = { selectedRouteIndex = it })
+                    onRouteSelected = {
+                      selectedRouteIndex = it
 
-                DrawGpsToActivityPath(gpsRouteDetails)
+                      bottomSheetContent = {
+                        LegDetailsContent(activitiesRouteDetails, it, validActivities)
+                      }
+                      scope.launch { bottomSheetScaffoldState.bottomSheetState.expand() }
+                    })
+
+                DrawGpsToActivityPath(
+                    gpsRouteDetails = gpsRouteDetails,
+                    onRouteSelected = {
+                      bottomSheetContent = { GpsDetailsContent(gpsRouteDetails, selectedActivity) }
+                      scope.launch { bottomSheetScaffoldState.bottomSheetState.expand() }
+                    })
               }
 
           // Add a floating action button for going back
@@ -156,7 +207,143 @@ fun ActivitiesMapScreen(
                 Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = "Go Back")
               }
         }
-      })
+      }
+}
+
+@Composable
+fun GpsDetailsContent(gpsRouteDetails: RouteDetails?, selectedActivity: Activity?) {
+  // Distance and duration details from GPS to the selected activity
+  val distance = gpsRouteDetails?.legsDistance?.firstOrNull() ?: "N/A"
+  val duration = gpsRouteDetails?.legsDuration?.firstOrNull() ?: "N/A"
+
+  Column(modifier = Modifier.fillMaxWidth().padding(16.dp)) {
+    Text(
+        text = "Your current Location",
+        style = MaterialTheme.typography.headlineSmall,
+        color = MaterialTheme.colorScheme.primary,
+        modifier = Modifier.padding(bottom = 8.dp))
+
+    Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.fillMaxWidth()) {
+      Divider(
+          color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.5f),
+          modifier = Modifier.width(1.dp).height(60.dp) // Increased height for better alignment
+          )
+
+      Spacer(modifier = Modifier.width(8.dp))
+
+      Column {
+        DetailRow(icon = Icons.Default.LocationOn, text = "Distance: $distance")
+        DetailRow(icon = Icons.Default.AccessTime, text = "Duration: $duration")
+      }
+    }
+
+    // Display selected activity details
+    selectedActivity?.let {
+      Text(
+          text = it.title,
+          style = MaterialTheme.typography.headlineSmall,
+          color = MaterialTheme.colorScheme.primary,
+          modifier = Modifier.padding(bottom = 8.dp))
+    }
+  }
+}
+
+@Composable
+fun ActivityDetailsContent(activity: Activity) {
+  // Date formatter
+  val dateFormatter = SimpleDateFormat("dd:MM:yy", Locale.getDefault())
+  val formattedDate = dateFormatter.format(activity.date.toDate())
+
+  // Time formatter
+  val timeFormatter = SimpleDateFormat("HH:mm", Locale.getDefault())
+  val formattedTime = timeFormatter.format(activity.date.toDate())
+
+  Column(modifier = Modifier.fillMaxWidth().padding(16.dp)) {
+    // Title Section
+    Text(
+        text = activity.title,
+        style = MaterialTheme.typography.headlineSmall,
+        color = MaterialTheme.colorScheme.primary,
+        modifier = Modifier.padding(bottom = 12.dp))
+
+    // Divider
+    Divider(
+        color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.2f),
+        modifier = Modifier.padding(bottom = 12.dp))
+
+    // Date Row
+    DetailRow(icon = Icons.Default.CalendarToday, text = formattedDate)
+
+    // Time Row
+    DetailRow(icon = Icons.Default.AccessTime, text = formattedTime)
+
+    // Location Row
+    DetailRow(icon = Icons.Default.LocationOn, text = activity.location.name)
+
+    // Description Row
+    DetailRow(icon = Icons.Default.Description, text = activity.description)
+  }
+}
+
+@Composable
+fun DetailRow(icon: ImageVector, text: String) {
+  Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.padding(bottom = 8.dp)) {
+    Icon(
+        imageVector = icon,
+        contentDescription = null,
+        tint = MaterialTheme.colorScheme.secondary,
+        modifier = Modifier.size(20.dp))
+    Spacer(modifier = Modifier.width(8.dp))
+    Text(
+        text = text,
+        style = MaterialTheme.typography.bodyMedium,
+        color = MaterialTheme.colorScheme.onBackground)
+  }
+}
+
+@Composable
+fun LegDetailsContent(routeDetails: RouteDetails?, legIndex: Int, activities: List<Activity>) {
+  val startActivity = activities.getOrNull(legIndex)
+  val endActivity = activities.getOrNull(legIndex + 1)
+  val distance = routeDetails?.legsDistance?.getOrNull(legIndex) ?: "N/A"
+  val duration = routeDetails?.legsDuration?.getOrNull(legIndex) ?: "N/A"
+
+  Column(modifier = Modifier.fillMaxWidth().padding(16.dp)) {
+    // Start Activity Title
+    startActivity?.let {
+      Text(
+          text = it.title,
+          style = MaterialTheme.typography.headlineSmall,
+          color = MaterialTheme.colorScheme.primary,
+          modifier = Modifier.padding(bottom = 8.dp))
+    }
+
+    // Row for Divider and Details
+    Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.fillMaxWidth()) {
+      // Vertical Divider
+      Divider(
+          color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.5f),
+          modifier = Modifier.width(1.dp).height(60.dp) // Increased height for better alignment
+          )
+
+      Spacer(modifier = Modifier.width(8.dp))
+
+      // Column for Distance and Duration
+      Column {
+        DetailRow(icon = Icons.Default.LocationOn, text = "Distance: $distance")
+        DetailRow(icon = Icons.Default.AccessTime, text = "Duration: $duration")
+      }
+    }
+
+    // End Activity Title
+    endActivity?.let {
+      Text(
+          text = it.title,
+          style = MaterialTheme.typography.headlineSmall,
+          color = MaterialTheme.colorScheme.primary,
+          modifier = Modifier.padding(top = 8.dp))
+    }
+  }
 }
 
 @Composable
@@ -177,7 +364,7 @@ fun AddActivityMarkers(
     activities: List<Activity>,
     dateFormat: SimpleDateFormat,
     selectedActivity: Activity?,
-    onActivitySelected: (Activity?) -> Unit
+    onActivitySelected: (Activity) -> Unit
 ) {
   Log.d("ActivitiesMapScreen", "Adding activity markers")
   activities.forEachIndexed { index, activity ->
@@ -189,7 +376,7 @@ fun AddActivityMarkers(
         snippet = dateFormat.format(activity.date.toDate()),
         icon = icon,
         onClick = {
-          onActivitySelected(if (selectedActivity == activity) null else activity)
+          onActivitySelected(activity)
           true
         })
   }
@@ -238,10 +425,15 @@ fun DrawActivitiesPaths(
 }
 
 @Composable
-fun DrawGpsToActivityPath(gpsRouteDetails: RouteDetails?) {
+fun DrawGpsToActivityPath(gpsRouteDetails: RouteDetails?, onRouteSelected: () -> Unit) {
   Log.d("ActivitiesMapScreen", "Drawing GPS to activity path")
   gpsRouteDetails?.legsRoute?.forEach { points ->
-    Polyline(points = points, color = Color.Blue, width = 10f)
+    Polyline(
+        points = points,
+        clickable = true,
+        color = Color.Blue,
+        width = 10f,
+        onClick = { onRouteSelected() })
   }
 }
 
