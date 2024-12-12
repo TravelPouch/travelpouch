54e6c0e1786baf8540a47e749ed9481f9d72db73
Arthur Chalard <arthur.chalard@epfl.ch>
2024-12-12 14:42:04 +0100

    fix: imports in SignIn screen

--- app/src/main/java/com/github/se/travelpouch/ui/authentication/SignIn.kt
+++ app/src/main/java/com/github/se/travelpouch/ui/authentication/SignIn.kt
@@ -23,10 +23,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.StrokeCap
@@ -51,6 +49,7 @@ import com.google.firebase.auth.auth
 import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
 
 /**
  * A composable function that displays the sign-in screen.
